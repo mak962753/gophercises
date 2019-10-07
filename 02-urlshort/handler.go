8b5c5f04ff32b6e95423141170c858595d2eda10
mak962753 <mak962753@gmail.com>
2019-10-07 17:58:44 +0300

    urlshorter - yaml, json

--- 02-urlshort/handler.go
+++ 02-urlshort/handler.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"encoding/json"
 	"fmt"
 	yaml2 "gopkg.in/yaml.v2"
 	"net/http"
@@ -17,13 +18,27 @@ func MapHandler(paths map[string]string, fallback http.Handler) http.HandlerFunc
 		}
 	}
 }
+func JSONHandler(jsonData []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var parsed []entry
+	err := json.Unmarshal(jsonData, &parsed)
+	fmt.Println("JSONHandler(): initialized", parsed)
+	if err != nil {
+		return nil, err
+	}
+	paths := map[string]string{}
+	for _, e := range parsed {
+		paths[e.Path] = e.Url
+	}
+	return MapHandler(paths, fallback), nil
+}
 
 // YAML is expected to be in the format:
 //
 //     - path: /some-path
 //       url: https://www.some-url.com/demo
 type entry struct {
-	path, url string
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
 }
 
 func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
@@ -35,7 +50,7 @@ func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	fmt.Println("YAMLHandler(): initialized", parsed)
 	paths := map[string]string{}
 	for _, e := range parsed {
-		paths[e.path] = e.url
+		paths[e.Path] = e.Url
 	}
 
 	return MapHandler(paths, fallback), nil
--- 02-urlshort/main/main.go
+++ 02-urlshort/main/main.go
@@ -4,37 +4,73 @@ package main
 // go get gopkg.in/yaml.v2
 
 import (
+	"flag"
 	"fmt"
 	urlshort "gophercises/02-urlshort"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("test")
+	yamlFile := flag.String("yaml", "", "if set specifies YAML file that contain url mappings.")
+	jsonFile := flag.String("json", "", "if set specifies YAML file that contain url mappings.")
 
+	flag.Parse()
+
+	mux := defaultMux()
+
+	handler := addMapHander(mux)
+	handler = addYamlHandler(yamlFile, handler)
+	handler = addJsonHandler(jsonFile, handler)
+
+	fmt.Println("Starting HTTP server on :8080... ")
+
+	_ = http.ListenAndServe(":8080", handler)
+}
+
+func addMapHander(fallback http.Handler) http.HandlerFunc {
 	paths := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-
-	mux := defaultMux()
-
-	handler := urlshort.MapHandler(paths, mux)
-	yaml := `
+	return urlshort.MapHandler(paths, fallback)
+}
+func addYamlHandler(file *string, fallback http.HandlerFunc) http.HandlerFunc {
+	const yamlUrls = `
 - path: /urlshort
   url: https://github.com/gophercises/urlshort
 - path: /urlshort-final
   url: https://github.com/gophercises/urlshort/tree/solution
 `
-	if yamlHandler, yamlErr := urlshort.YAMLHandler([]byte(yaml), handler); yamlErr == nil {
-		handler = yamlHandler
-	} else {
-		fmt.Println("Failed to initialize YAML map handler.", yamlErr)
+	data := []byte(yamlUrls)
+	if file != nil {
+		var err error
+		data, err = ioutil.ReadFile(*file)
+		if err != nil {
+			fmt.Println("addYamlHandler: Failed to read yaml file", err)
+			return fallback
+		}
 	}
+	yamlHandler, yamlErr := urlshort.YAMLHandler(data, fallback)
+	if yamlErr == nil {
+		return yamlHandler
+	}
+	fmt.Println("addYamlHandler: to initialize YAML map handler.", yamlErr)
+	return fallback
+}
 
-	fmt.Println("Starting HTTP server on :8080... ")
-
-	_ = http.ListenAndServe(":8080", handler)
+func addJsonHandler(file *string, fallback http.HandlerFunc) http.HandlerFunc {
+	data, err := ioutil.ReadFile(*file)
+	if err != nil {
+		fmt.Println("addJsonHandler: Failed to read json file", err)
+		return fallback
+	}
+	jsonHandler, err := urlshort.JSONHandler(data, fallback)
+	if err != nil {
+		fmt.Println("addJsonHandler: Failed to initialize json handler", err)
+		return fallback
+	}
+	return jsonHandler
 }
 
 func defaultMux() *http.ServeMux {
